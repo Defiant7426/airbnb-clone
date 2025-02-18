8b173140a07ec0c81ab303c23f7f3a38436065a1
Defiant7426 <pldelacruzv@gmail.com>
2025-02-18 16:20:02 -0500

    Creando el index de la pagina publica

--- airbnb-clone/src/App.tsx
+++ airbnb-clone/src/App.tsx
@@ -1,12 +1,11 @@
+import Index from "./page/Index"
 
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline bg-red-400">
-    Hello world!
-  </h1>
+      <Index />
     </>
   )
 }
